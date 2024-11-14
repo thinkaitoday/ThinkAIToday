7c4bcc25aecc91689f1192be32c92fc16c37d51a
NR <nr@gmail.com>
2024-11-14 15:56:40 -0500

    Add the timer for flash message on homescreen

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -23,6 +23,13 @@ import { CommonModule } from '@angular/common';  // Import CommonModule
 export class HomeComponent {
   flashNews: boolean = true;
 
+  ngOnInit() {
+    // Automatically dismiss the flash message after 10 seconds
+    setTimeout(() => {
+      this.flashNews = false;
+    }, 10000);
+  }
+
   missionStatement: string = `
     At Think AI Today, we are dedicated to making knowledge of Artificial Intelligence, 
     especially Generative AI, accessible to everyone. We believe that in today’s rapidly 
